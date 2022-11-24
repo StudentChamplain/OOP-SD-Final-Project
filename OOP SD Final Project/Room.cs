53bb03846bf8a2f5519d251ed82c73197548cdcc
208826 <LE2088826@crc-lennox.qc.ca>
2022-11-24 12:14:57 -0500

    Added properties to Room.cs.

--- OOP SD Final Project/Room.cs
+++ OOP SD Final Project/Room.cs
@@ -8,6 +8,19 @@ namespace OOP_SD_Final_Project
 {
     internal class Room
     {
+        private int roomId;
+        private string roomName;
+        private int maxSeats;
 
+        public Room()
+        {
+
+        }
+
+        public int RoomId { get { return roomId; } }
+
+        public string RoomName { get { return roomName; } set { roomName = value; } }
+
+        public int MaxSeats { get { return maxSeats; } set { maxSeats = value; } }
     }
 }
