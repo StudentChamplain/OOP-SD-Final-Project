0f508cea830a5548cd2034b1ed69bedf89f7f54b
208826 <LE2088826@crc-lennox.qc.ca>
2022-11-24 11:22:14 -0500

    Created Room class.

---
+++ OOP SD Final Project/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_SD_Final_Project
+{
+    internal class Room
+    {
+
+    }
+}
